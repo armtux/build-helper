8a07fa9ee0779da10cd876b4a2d1ed8136d310a4
hairu <>
2023-08-16 02:58:53 +0000

    remove custom mirror settings

--- scripts/example-starter.sh
+++ scripts/example-starter.sh
@@ -7,7 +7,6 @@ STARTER_PATH=$(readlink -f "$0")
 cd $(dirname "${STARTER_PATH}")
 [ ! -e ../logs ] && mkdir -p ../logs
 
-TARBALL_MIRROR="https://mirror.csclub.uwaterloo.ca/gentoo-distfiles" \
 MNT_TYPE="bind" HIST_TYPE="squashfs" TMP_TYPE="tmpfs" \
 ./build-helper.sh aarch64-unknown-linux-musl rpi4b 2>&1 \
 $([ "${TERM_PROGRAM}" != "tmux" ] && echo -n "| tee ../logs/rpi4b-${STARTER_TIME}.log")
